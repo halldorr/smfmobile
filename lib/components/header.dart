96eda8c12439137e67d097d89bac9a243c8c039c
Jeff Lewis <474547+halldorr@users.noreply.github.com>
2020-01-04 17:54:24 -0500

    Change header to a card

--- lib/components/header.dart
+++ lib/components/header.dart
@@ -4,34 +4,36 @@ class Header extends StatelessWidget {
   final searchController = TextEditingController();
 
   Widget build(context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Image.network(
-            'https://avatars.simplemachinesweb.com/smf/avatar_17.png'),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                'Hello Jeff Lewis',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 15.0,
-                  fontWeight: FontWeight.bold,
+    return Card(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Image.network(
+              'https://avatars.simplemachinesweb.com/smf/avatar_17.png'),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Hello Jeff Lewis',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Text(
-                'January 04, 2020, 03:33:33PM',
-                style: TextStyle(
-                  color: Colors.black,
-                ),
-              )
-            ],
+                Text(
+                  'January 04, 2020, 03:33:33PM',
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
